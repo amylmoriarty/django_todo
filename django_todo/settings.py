81eacabb4b003e3065d844ec27d430b94f984798
Amy Moriarty <lostjrt16@gmail.com>
2019-04-01 00:22:17 +0000

    Implement enviroment specific values for the debug and database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+if os.environ.get ('DEVELOPMENT'):
+    development = True
+
+else: 
+    development = False
+
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'on858uze0$v$@60tz-_r3w6qu)m35=+)t6_k2n$y)a@7-l63j5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = [os.environ.get ('C9_HOSTNAME'),
                 os.environ.get('HOSTNAME')]
@@ -76,15 +84,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-#DATABASES = {
-#       'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
+if development:
+    DATABASES = {
+       'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
 
-DATABASES = { 'default':dj_database_url.parse(os.environ.get ('DATABASE_URL'))}
+    DATABASES = { 'default':dj_database_url.parse(os.environ.get ('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
